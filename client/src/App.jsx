cde6a1a3972b1c7b6321a57ff401aed215ce837b
tathagatamishra <tathsgatamishra007@gmail.com>
2023-09-17 02:13:19 +0530

    unwanted code removed (input output)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.scss";
 
@@ -6,19 +6,9 @@ import Header from "./Component/Header/Header";
 import Action from "./Component/Action/Action";
 import Footer from "./Component/Footer/Footer";
 
-// import Input from "./Component/Input/Input";
-// import Output from "./Component/Output/Output";
-
 import { DarkModeProvider } from "./assets/DarkModeContext";
 
 export default function App() {
-
-  // const [outputData, setOutputData] = useState([]);
-
-  // Callback function to receive data from the child component
-  // const receiveInputData = (data) => {
-  //   setOutputData(data);
-  // };
 
   return (
     <DarkModeProvider>
@@ -26,8 +16,6 @@ export default function App() {
         <Header />
         <div className="webBody">
           <Action />
-          {/* <Input dataForParent={receiveInputData} /> */}
-          {/* <Output dataFromParent={outputData} /> */}
         </div>
         <Footer />
       </div>
